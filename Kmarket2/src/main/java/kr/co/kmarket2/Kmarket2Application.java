9bc9d02cf55e06d1ee532e85865c121a163b11de
KimDongGeun200418 <chie44911@gmail.com>
2023-02-07 10:13:56 +0900

    Feat : Make Project

---
+++ Kmarket2/src/main/java/kr/co/kmarket2/Kmarket2Application.java
@@ -0,0 +1,13 @@
+package kr.co.kmarket2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kmarket2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kmarket2Application.class, args);
+	}
+
+}
