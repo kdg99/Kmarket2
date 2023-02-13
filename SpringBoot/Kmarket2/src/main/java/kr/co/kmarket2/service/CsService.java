d80ca827ab3449bf20922a15d47f49c55009fb65
Chocokano <111360218+Chocokano@users.noreply.github.com>
2023-02-13 13:25:06 +0900

    cs-qna 작업중

---
+++ SpringBoot/Kmarket2/src/main/java/kr/co/kmarket2/service/CsService.java
@@ -0,0 +1,98 @@
+/*
+날짜 : 2023/02/13
+이름 : 최아영
+내용 : Kmarket2 STS CsSevice
+*/
+package kr.co.kmarket2.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.kmarket2.dao.CsDAO;
+import kr.co.kmarket2.vo.Cs_Cate1VO;
+import kr.co.kmarket2.vo.Cs_QnaVO;
+
+
+@Service
+public class CsService {
+		
+		@Autowired
+		private CsDAO dao;
+		
+		public Cs_Cate1VO selectCate1(String cate1){
+	        return dao.selectCate1(cate1);
+	    }
+		
+		/* index */
+				
+		/* Notice */
+		
+		/* Faq */
+		
+		/* Qna */
+		public int insertQnaArticle(Cs_QnaVO vo){
+			
+	        int result = dao.insertQnaArticle(vo);
+	        return result;
+	    }
+		
+		public Cs_QnaVO selectQnaArticle(int no){
+	        return dao.selectQnaArticle(no);
+	    }
+		
+		public List<Cs_QnaVO> selectQnaArticles(int start, String cate1){
+	        return dao.selectQnaArticles(start, cate1);
+	    }
+		
+		public Cs_QnaVO selectCsQnaNo(int no){
+	        return dao.selectCsQnaNo(no);
+	    }
+
+		public int getLimitStart(int currentPage){
+	        return (currentPage - 1) * 10;
+	    }
+		
+		public int getCurrentPage(String pg){
+		        int currentPage = 1;
+		        if (pg != null){
+		            currentPage = Integer.parseInt(pg);
+		        }
+		        return currentPage;
+		 }
+		
+		public long getTotalCount(String cate1){
+		        return dao.selectCountTotal(cate1);
+		}
+		
+		public int getLastPageNum(long total){
+	        int lastPage = 0;
+
+	        if (total % 10 == 0){
+	            lastPage = (int) (total/10);
+	        }else{
+	            lastPage = (int) ((total/10) + 1);
+	        }
+	        return lastPage;
+	    }
+		
+		public int getPageStartNum(long total, int start){
+		        return (int) (total - start);
+		}
+		
+		public int[] getPageGroup(int currentPage, int lastPage){
+	        int groupCurrent = (int) Math.ceil(currentPage/10.0);
+	        int groupStart = (groupCurrent - 1) * 10 + 1;
+	        int groupEnd = groupCurrent * 10;
+
+	        if (groupEnd > lastPage){
+	            groupEnd = lastPage;
+	        }
+
+	        int[] groups = {groupStart, groupEnd};
+
+	        return groups;
+	    }
+		
+}
