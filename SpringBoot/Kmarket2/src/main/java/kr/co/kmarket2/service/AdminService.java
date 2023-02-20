61df69d376f91c1de7a4234972cbfe4080712d52
Hoongithub2022 <gns2506@gmail.com>
2023-02-20 13:29:21 +0900

    2023-02-20 Admin

--- SpringBoot/Kmarket2/src/main/java/kr/co/kmarket2/service/AdminService.java
+++ SpringBoot/Kmarket2/src/main/java/kr/co/kmarket2/service/AdminService.java
@@ -5,10 +5,19 @@
  * */
 package kr.co.kmarket2.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 
+import ch.qos.logback.core.model.Model;
+import jakarta.transaction.Transactional;
 import kr.co.kmarket2.dao.AdminDAO;
+import kr.co.kmarket2.utils.PageHandler;
+import kr.co.kmarket2.utils.SearchCondition;
+import kr.co.kmarket2.vo.MemberVO;
+import kr.co.kmarket2.vo.ProductVO;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -17,4 +26,39 @@ public class AdminService {
 	@Autowired
 	private AdminDAO dao;
 	
+	// 상품 등록
+	@Transactional
+	public int insertProductAdmin(ProductVO product) {
+		// 파일 업로드
+		FileUpload(product);
+		// 상품 등록
+		int result = dao.insertProductAdmin(product);
+		return result;
+	}
+
+	// 상품 조회
+	public void selectProductAdmin(Model m, SearchCondition sc, @AuthenticationPrincipal MemberVO user) {
+        int totalCnt = dao.countProductAdmin(sc); // 전체 상품 갯수
+
+        /** 검색 페이지 > 전체 페이수일 경우 실행 **/
+        int totalPage = (int)Math.ceil(totalCnt/(double)sc.getPageSize()); // 전체 페이지수
+
+        // 전체 페이지수가 현재 페이지수 보다 크면 전체 페이지수로 값 저장
+        if(sc.getPage() > totalPage) sc.setPage(totalPage);
+
+        PageHandler pageHandeler = new PageHandler(totalCnt, sc); // 페이징 처리
+
+        // SearchCondition에 uid, type 담기
+        sc.setType(user.getType());
+        sc.setUid(user.getUid());
+        System.out.println("type : " + user.getType());
+
+        List<ProductVO> list = dao.selectProductAdmin(sc); // 상품 조회
+	}
+	
+	
+	private void FileUpload(ProductVO product) {
+		
+		
+	}
 }
---
+++ SpringBoot/Kmarket2/src/main/java/kr/co/kmarket2/utils/SearchCondition.java
@@ -0,0 +1,65 @@
+package kr.co.kmarket2.utils;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchCondition {
+    private Integer page = 1;
+    private Integer pageSize = 10;
+    private String cate1;
+    private String cate2;
+    private Integer no = 0;
+    private String searchField;
+    private String searchWord;
+
+    // 추가 필드
+    private int type;
+    private String uid;
+
+
+    public String getQueryString(Integer page){
+        // ?page=1&pageSize=10&option="T"&keyword="title"
+        return getQueryString(page, no);
+    }
+
+    public String getQueryString(Integer page, Integer no){
+        // ?page=1&pageSize=10&option="T"&keyword="title"
+        UriComponentsBuilder builder = UriComponentsBuilder.newInstance()
+                .queryParam("cate1", cate1)
+                .queryParam("cate2", cate2)
+                .queryParam("page", page);
+
+
+        if (no != null && no != 0)
+            builder.queryParam("no", no);
+
+        // 검색 기능
+        if(searchField != null && !searchWord.isBlank()){
+            builder.queryParam("searchField", searchField)
+                    .queryParam("searchWord", searchWord);
+        }
+
+        return builder.toUriString();
+    }
+
+    public String getQueryString(){
+        // ?page=1&pageSize=10&option="T"&keyword="title"
+        return getQueryString(page);
+    }
+
+    // limit
+    public Integer getOffset() {
+        return (page-1) * pageSize;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page == 0 ? 1:page;
+    }
+
+}
