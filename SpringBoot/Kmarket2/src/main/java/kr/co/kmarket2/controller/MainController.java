4e5875fb4faeba1fe32375e2509e8fc42622abd7
KimDongGeun200418 <chie44911@gmail.com>
2023-02-24 10:20:58 +0900

    Feat:Update TermsPolicy

--- SpringBoot/Kmarket2/src/main/java/kr/co/kmarket2/controller/MainController.java
+++ SpringBoot/Kmarket2/src/main/java/kr/co/kmarket2/controller/MainController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import kr.co.kmarket2.service.MainService;
+import kr.co.kmarket2.service.MemberService;
 import kr.co.kmarket2.vo.ProductVO;
+import kr.co.kmarket2.vo.TermsVO;
 
 @MapperScan("kr.co.kmarket2.dao")
 @Controller
@@ -17,6 +19,8 @@ public class MainController {
 	
 	@Autowired
 	private MainService service;
+	@Autowired
+	private MemberService serviceMember;
 	
 	
 	@GetMapping(value = {"/", "index"})
@@ -37,15 +41,67 @@ public class MainController {
     }
 	
 	
-	@GetMapping(value = {"termsPolicy/", "termsPolicy/index"})
+	
+	// termsPolicy
+	@GetMapping(value = {"termsPolicy/", "termsPolicy/index", "termsPolicy/buyer"})
     public String termsIndex(Model model){
-		return "termsPolicy/index";
+		TermsVO terms = serviceMember.selectTerms();
+		model.addAttribute("terms", terms.getTerms());
+		return "termsPolicy/buyer";
+	}
+	@GetMapping("termsPolicy/seller")
+    public String termsSeller(Model model){
+		TermsVO terms = serviceMember.selectTerms();
+		model.addAttribute("terms", terms.getTax());
+		return "termsPolicy/seller";
+	}
+	@GetMapping("termsPolicy/finance")
+    public String termsFinance(Model model){
+		TermsVO terms = serviceMember.selectTerms();
+		model.addAttribute("terms", terms.getFinance());
+		return "termsPolicy/finance";
+	}
+	@GetMapping("termsPolicy/location")
+    public String termsLocation(Model model){
+		TermsVO terms = serviceMember.selectTerms();
+		model.addAttribute("terms", terms.getLocation());
+		return "termsPolicy/location";
+	}
+	@GetMapping("termsPolicy/privacy")
+    public String termsPrivacy(Model model){
+		TermsVO terms = serviceMember.selectTerms();
+		model.addAttribute("terms", terms.getPrivacy());
+		return "termsPolicy/privacy";
 	}
 	
 	
+	
+	
+	
+	// company
 	@GetMapping(value = {"company/", "company/index"})
     public String companyIndex(Model model){
 		return "company/index";
+	}
+	
+	@GetMapping("company/introduce")
+    public String companyIntroduce(Model model){
+		return "company/introduce";
+	}
+	
+	@GetMapping("company/manage")
+    public String companyManage(Model model){
+		return "company/manage";
+	}
+	
+	@GetMapping("company/notice")
+    public String companyNotice(Model model){
+		return "company/notice";
+	}
+	
+	@GetMapping("company/promote")
+    public String companyPromote(Model model){
+		return "company/promote";
 	}
 	
 	
